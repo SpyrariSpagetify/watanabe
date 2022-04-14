ca1902743330c25a88837dc145444e855a69ca70
meatward <mad4brain@gmail.com>
2022-04-14 15:47:55 +0300

    just

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TodoHeader from './../TodoHeader/TodoHeader';
 import TodoActions from './../TodoActions/TodoActions';
 import TodoRender from './../TodoRender/TodoRender';
 import './Todo.css';
 
+const initialFormData = {
+  isEdit: false,
+  todoName:'',
+  text:'',
+  isDone: false,
+  id: '',
+  index: null
+}
 
 const Todo = ()=>{
+  const [tab, setTab] = useState(0)
+  const [isOpen, setIsOpen] = useState(false)
+  const [isOpenDisplay, setIsOpenDisplay] = useState(false)
+  const [todos, setTodos] = useState([]) 
+  const [formData, setFormData] = useState(initialFormData)
+
+  const handleOpenDialog = () => setIsOpen((prev)=>!prev)
+
   return (
     <div className='todo-wrapper'>
       <TodoHeader />
--- src/components/TodoHeader/TodoHeader.js
+++ src/components/TodoHeader/TodoHeader.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import './TodoHeader.css'
+import moment from 'moment';
+import AddToPhotosIcon from '@mui/icons-material/AddToPhotos';
+
+import './TodoHeader.css';
 
 const TodoHeader = ()=>{
+    const weekDay = moment().format('LL');
+    const date = moment().date();
+
     return(
-        <div>TodoHeader</div>
+        <div>
+            <div>
+                <div className='todos-count'>
+                    <span></span>
+                    <div>
+                        <span>Tasks</span>
+                        <span>total count</span>
+                    </div>
+                    <div>
+                        <span>{weekDay}</span>
+                        <span>{date}</span>
+                    </div>
+                </div>
+                <div className='add-todo'>
+                    <AddToPhotosIcon color='error' />
+                </div>
+            </div>
+        </div>
     )
 }
 
